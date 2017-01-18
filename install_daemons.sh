c3f1d26bb229a97bda418895ace77c324e18ecdd
Chris Friedrich <cnf@uoregon.edu>
2017-01-18 07:37:46 +0000

    Fixed installation script issues. Added empty directory.

--- install_daemons.sh
+++ install_daemons.sh
@@ -4,10 +4,12 @@
 
 # Setup postgres
 git clone -b "REL9_5_STABLE" "https://github.com/postgres/postgres.git"
-cd postgres/postgres
+cd postgres
 ./configure --prefix=$1
 make
 make install
+
+cd ..
 
 # Setup apache
 curl http://www-us.apache.org/dist//httpd/httpd-2.4.25.tar.gz > apache-2-4-25.tar.gz 

aae1d13ea9ad050f0b8b705ab0902f2f8afb26e3
Chris Friedrich <cnf@uoregon.edu>
2017-01-20 06:20:04 +0000

    Added sql directory

---
+++ sql/create_tables.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE products (
+	product_pk		serial primary key,
+	vendor			varchar(128),
+	description		varchar(256),
+	alt_description		varchar(256)
+)
+
+CREATE TABLE assets (
+	asset_pk
+	
